6f277d0535648c720db6ee155ef24c816afd0c24
LucasVidela <videlalucasandres@gmail.com>
2021-11-16 11:45:29 -0300

    ABM categorias

--- resources/controller/categorias.py
+++ resources/controller/categorias.py
@@ -1,4 +1,4 @@
-from flask import jsonify, Blueprint
+from flask import jsonify, Blueprint, request
 from resources.service import categorias as categories
 from resources.service.usuarios import token_required
 
@@ -9,3 +9,17 @@ categorias_bp = Blueprint("routes-categories", __name__)
 # @token_required
 def all_categories():
     return jsonify({"productos": categories.get_all_categories()}), 200
+
+
+@categorias_bp.route('/categorias', methods=['POST'])
+# @token_required
+def add_category():
+    id_categoria = categories.add_category(request.json)
+    return jsonify({"idcategoria": id_categoria}), 200
+
+
+@categorias_bp.route('/categorias/<int:id_categoria>', methods=['PUT'])
+# @token_required
+def update_category(id_categoria):
+    categories.update_category(id_categoria, request.json)
+    return jsonify({"message": "Editado!"}), 200
--- resources/service/categorias.py
+++ resources/service/categorias.py
@@ -5,13 +5,46 @@ from database.connection import create_connection
 
 def get_all_categories():
     conn = create_connection()
-    sql = f"SELECT * FROM categorias"
+    sql = f"SELECT * FROM categorias ORDER BY categoria ASC;"
 
     try:
         cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
         cur.execute(sql)
         products = cur.fetchall()
         return [dict(p) for p in products]
+    except Error as e:
+        print(str(e))
+    finally:
+        if conn:
+            conn.close()
+
+
+def add_category(request):
+    conn = create_connection()
+    sql = f"INSERT INTO categorias(categoria) VALUES ('{request['categoria'].upper()}') RETURNING id;"
+
+    try:
+        cur = conn.cursor()
+        cur.execute(sql)
+        conn.commit()
+        id_categoria = cur.fetchone()[0]
+        return id_categoria
+    except Error as e:
+        print(str(e))
+    finally:
+        if conn:
+            conn.close()
+
+
+def update_category(_id, request):
+    conn = create_connection()
+    sql = f"UPDATE categorias SET categoria='{request['categoria'].upper()}' WHERE id='{_id}';"
+
+    try:
+        cur = conn.cursor()
+        cur.execute(sql)
+        conn.commit()
+        return
     except Error as e:
         print(str(e))
     finally:
