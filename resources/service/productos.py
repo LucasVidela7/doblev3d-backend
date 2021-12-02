4c375562aba81be6d322d5368e7a15197f749a37
LucasVidela <videlalucasandres@gmail.com>
2021-12-02 17:42:49 -0300

    listado de productos

--- resources/service/productos.py
+++ resources/service/productos.py
@@ -41,7 +41,7 @@ def get_all_products():
     sql = f"SELECT p.*, cats.categoria AS idcategoria, " \
           f"(SELECT count(id) FROM ventas_productos WHERE idproducto=p.id " \
           f"and idestado<>(SELECT id FROM estados where productos='1' ORDER BY id DESC LIMIT 1 OFFSET 0)) AS ventas, " \
-          f"(SELECT max(COALESCE(precioUnitario,0)) FROM precio_unitario WHERE idproducto=p.id ORDER BY id DESC LIMIT 1 OFFSET 0) as precioUnitario " \
+          f"(SELECT precioUnitario FROM precio_unitario WHERE idproducto=p.id ORDER BY id DESC LIMIT 1 OFFSET 0) as precioUnitario " \
           f"FROM productos AS p " \
           f"INNER JOIN categorias as cats ON cats.id = p.idcategoria " \
           f"ORDER BY p.estado DESC, p.id DESC;"
