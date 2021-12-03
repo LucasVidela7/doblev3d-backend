eec963278838b8587c1a04ef46b70026a313f6c7
LucasVidela <videlalucasandres@gmail.com>
2021-12-03 16:51:12 -0300

    cancelar productos

--- resources/service/estados.py
+++ resources/service/estados.py
@@ -133,6 +133,8 @@ def cancelar_producto(id_producto):
     db.update_sql(sql)
 
     sql = f"select avg(idestado) as estado from ventas where id='{id_venta}';"
+    print(int(db.select_first(sql)["estado"]))
+    print(int(estado_cancelar))
     if int(estado_cancelar) == int(db.select_first(sql)["estado"]):
-        sql = f"update ventas set idestado='{estado_cancelar}'"
+        sql = f"update ventas set idestado='{estado_cancelar}' where id='{id_venta}'"
         db.update_sql(sql)
