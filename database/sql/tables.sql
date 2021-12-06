e149d5a41bb471af7e81dec88ee3d98b6cd9216f
LucasVidela <videlalucasandres@gmail.com>
2021-12-06 20:57:46 -0300

    gastos

--- database/sql/tables.sql
+++ database/sql/tables.sql
@@ -101,7 +101,7 @@ CREATE TABLE IF NOT EXISTS gastos(
     id SERIAL PRIMARY KEY,
     monto FLOAT NOT NULL,
     descripcion TEXT,
-    fechaGasto DATE,
+    fechaGasto DATE
 );
 
 DROP TABLE medios_pago;
