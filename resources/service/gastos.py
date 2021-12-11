e28e6ae0f229596a18e6b86f9cbe3bfc0eacaad6
LucasVidela <videlalucasandres@gmail.com>
2021-12-10 22:10:28 -0300

    gastos

--- resources/service/gastos.py
+++ resources/service/gastos.py
@@ -29,7 +29,7 @@ def get_gastos(mes=None, anio=None):
         anio = datetime.now().year
 
     sql = f"SELECT * from gastos " \
-          f"WHERE EXTRACT(month FROM 'fechagasto') = {mes} AND EXTRACT(year FROM 'fechagasto') = {anio};"
+          f"WHERE EXTRACT(month FROM fechagasto) = {mes} AND EXTRACT(year FROM fechagasto) = {anio};"
     gastos = db.select_multiple(sql)
     for g in gastos:
         g["fechagasto"] = g["fechagasto"].strftime('%Y-%m-%d')
