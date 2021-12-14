0ab42bd435331d0be58d281fdc1ae66acfd33c47
LucasVidela <videlalucasandres@gmail.com>
2021-12-14 12:02:08 -0300

    balance

--- resources/service/balance.py
+++ resources/service/balance.py
@@ -7,26 +7,26 @@ from resources.service.ventas import get_all_ventas
 
 def get_balance():
     # Ingresos
-    sql = f"SELECT date_trunc('month', fechapago) AS mes, COALESCE(sum(monto),0) as ingresos " \
+    sql = f"SELECT date_trunc('month', fechapago) AS mes, COALESCE(sum(monto),0) as monto " \
           f"FROM pagos GROUP BY mes order by mes desc;"
     meses_ingresos = db.select_multiple(sql)
 
     for m in meses_ingresos:
         m["mes"] = m["mes"].strftime('%m-%Y')
 
-    ingreso_total = sum([float(m["ingresos"]) for m in meses_ingresos])
+    ingreso_total = sum([float(m["monto"]) for m in meses_ingresos])
 
     # Otros
     ventas = get_all_ventas()
     falta_cobrar = sum([float(v["preciototal"] - v["senia"]) for v in ventas])
 
     # Gastos
-    sql = f"SELECT date_trunc('month', fechagasto) AS mes, COALESCE(sum(monto),0) as gastos " \
+    sql = f"SELECT date_trunc('month', fechagasto) AS mes, COALESCE(sum(monto),0) as monto " \
           f"FROM gastos GROUP BY mes order by mes desc;"
     meses_gastos = db.select_multiple(sql)
     for m in meses_gastos:
         m["mes"] = m["mes"].strftime('%m-%Y')
-    gastos_total = sum([float(m["gastos"]) for m in meses_gastos])
+    gastos_total = sum([float(m["monto"]) for m in meses_gastos])
 
     response = {}
     response["ingresoTotal"] = round(ingreso_total, 2)
@@ -34,6 +34,6 @@ def get_balance():
     response["faltaCobrar"] = round(falta_cobrar, 2)
     response["enCaja"] = round(ingreso_total - gastos_total, 2)
     response["meses"] = {"ingresos": meses_ingresos}
-    response["meses"] = {"gastos": meses_gastos}
+    response["meses"].update({"gastos": meses_gastos})
 
     return response
