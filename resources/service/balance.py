58bee37f5502e3fb951fcbc6404ce6049cbeb66c
LucasVidela <videlalucasandres@gmail.com>
2021-12-13 22:59:18 -0300

    balance

--- resources/service/balance.py
+++ resources/service/balance.py
@@ -4,11 +4,11 @@ from resources.service import estados as estados
 
 
 def get_balance():
-    sql = f"SELECT date_trunc('month', fechapago) AS mes, COALESCE(sum(monto),0) as monto " \
+    sql = f"SELECT date_trunc('month', fechapago) AS mes, COALESCE(sum(monto),0) as ingreso " \
           f"FROM pagos GROUP BY mes order by mes desc;"
     meses = db.select_multiple(sql)
 
-    ingreso_total = sum([float(m["monto"]) for m in meses])
+    ingreso_total = sum([float(m["ingreso"]) for m in meses])
     estado_cancelado = estados.get_id_estado_cancelado()
 
     sql = f"SELECT COALESCE(sum(preciounidad),0) as total FROM ventas_productos where idestado <> {estado_cancelado};"
