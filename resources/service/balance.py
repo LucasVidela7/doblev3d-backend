b0391d6c7a6bf62a5650dcf32097ba1769db6261
LucasVidela <videlalucasandres@gmail.com>
2021-12-13 23:22:17 -0300

    balance

--- resources/service/balance.py
+++ resources/service/balance.py
@@ -2,6 +2,7 @@ import locale
 from datetime import datetime
 from database import utils as db
 from resources.service import estados as estados
+from resources.service.ventas import get_all_ventas
 
 
 def get_balance():
@@ -15,8 +16,10 @@ def get_balance():
     ingreso_total = sum([float(m["ingreso"]) for m in meses])
     estado_cancelado = estados.get_id_estado_cancelado()
 
-    sql = f"SELECT COALESCE(sum(preciounidad),0) as total FROM ventas_productos where idestado <> {estado_cancelado};"
-    precio_unitario_total = float(db.select_first(sql)["total"])
+    # sql = f"SELECT COALESCE(sum(preciounidad),0) as total FROM ventas_productos where idestado <> {estado_cancelado};"
+    # precio_unitario_total = float(db.select_first(sql)["total"])
+    ventas = get_all_ventas()
+    falta_cobrar = sum([float(v["preciototal"] - v["senia"]) for v in ventas])
 
     sql = f"SELECT COALESCE(sum(monto),0) as total FROM gastos;"
     gastos_total = float(db.select_first(sql)["total"])
@@ -24,7 +27,7 @@ def get_balance():
     response = {}
     response["ingresoTotal"] = round(ingreso_total, 2)
     response["gastosTotal"] = round(gastos_total, 2)
-    response["faltaCobrar"] = max(round(precio_unitario_total - ingreso_total, 2), 0)
+    response["faltaCobrar"] = round(falta_cobrar, 2)
     response["enCaja"] = round(ingreso_total - gastos_total, 2)
     response["meses"] = meses
 
