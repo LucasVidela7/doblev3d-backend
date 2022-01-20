2b206e75b4e4230239844fc6b8c0a32ff5af0987
LucasVidela <videlalucasandres@gmail.com>
2022-01-19 23:13:44 -0300

    piezas

--- resources/service/ventas.py
+++ resources/service/ventas.py
@@ -86,33 +86,18 @@ def select_venta_by_id(_id):
 
     # Performance
     estados_productos = estados.get_estados_productos()
-    estados_piezas = estados.get_estados_piezas()
     ids_products = list(str(x["idproducto"]) for x in productos)
-    # sql = f"SELECT vpp.id as idpieza, vpp.idproductoventa, vpp.idestado, p.descripcion, p.horas, p.minutos " \
-    #       f"FROM ventas_productos_piezas AS vpp " \
-    #       f"INNER JOIN piezas AS p ON vpp.idpieza=p.id " \
-    #       f"WHERE vpp.idproductoventa in ({','.join(ids_products)});"
-
-    sql = f"select piezas.id as idpieza, * from piezas where idproducto in ({','.join(ids_products)});"
+    sql = f"select * from piezas where idproducto in ({','.join(ids_products)});"
     piezas = db.select_multiple(sql)
-    estado_listo = estados.get_id_estado_listo()
 
     for p in productos:
-        # Obtener piezas
-        # p["estado"] = estados.order_estados(copy.deepcopy(estados_productos), p["idestado"])
+        p["estado"] = estados.order_estados(copy.deepcopy(estados_productos), p["idestado"])
 
+        # Obtener piezas
         p["piezas"] = [pi for pi in piezas if pi["idproducto"] == p["idproducto"]]
         p.pop("idestado", None)
         p.pop("idventa", None)
         p.pop("idproducto", None)
-        # sum_piezas = 0
-        # for pi in p["piezas"]:
-        #     pi["estado"] = estados.order_estados(copy.deepcopy(estados_piezas), pi["idestado"])
-        #     sum_piezas += 1 if pi["idestado"] == estado_listo else 0
-        #     pi.pop("idestado", None)
-        #
-        # if sum_piezas != len(p['piezas']):
-        #     p["descripcion"] += f" ({sum_piezas}/{len(p['piezas'])})"
 
     venta["productos"] = productos
     return venta
