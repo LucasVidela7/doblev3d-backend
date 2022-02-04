0ffe369d8adfcc71b07bf7aa5b109eaef12f09ff
LucasVidela <videlalucasandres@gmail.com>
2022-02-04 11:26:49 -0300

    catalog

--- resources/controller/catalogo.py
+++ resources/controller/catalogo.py
@@ -6,14 +6,14 @@ catalogo_bp = Blueprint("routes-catalog", __name__)
 
 @catalogo_bp.route('/catalogo/categorias', methods=['GET'])
 def all_catalog_categories():
-    return jsonify({catalogo.get_all_categories_for_catalog()}), 200
+    return jsonify(catalogo.get_all_categories_for_catalog()), 200
 
 
 @catalogo_bp.route('/catalogo/productos/<int:id_categoria>', methods=['GET'])
 def all_products_by_id_category(id_categoria):
-    return jsonify({catalogo.get_all_products_for_catalog(id_categoria)}), 200
+    return jsonify(catalogo.get_all_products_for_catalog(id_categoria)), 200
 
 
 @catalogo_bp.route('/catalogo/productos/destacados', methods=['GET'])
-def all_featured_products(id_categoria):
-    return jsonify({catalogo.get_featured_products()}), 200
+def all_featured_products():
+    return jsonify(catalogo.get_featured_products()), 200
