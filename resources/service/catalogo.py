6e5e81e05d227c9675fb76ecdca0e20029f52510
videl <videlalucasandres@gmail.com>
2023-01-27 13:52:55 -0300

    refactor catalogo

--- resources/service/catalogo.py
+++ resources/service/catalogo.py
@@ -3,6 +3,7 @@ import pickle
 
 from database import utils as db
 from database.utils import redisx
+from resources.service.productos import get_all_products
 
 
 def get_all_categories_for_catalog():
@@ -17,28 +18,33 @@ def get_all_categories_for_catalog():
 
 
 def get_all_products_for_catalog(id_categoria):
-    sql = f"SELECT p.id, p.descripcion, cats.categoria AS categoria, " \
-          f"(SELECT count(id) FROM ventas_productos WHERE idproducto=p.id " \
-          f"and idestado<>(SELECT id FROM estados where productos='1' ORDER BY id DESC LIMIT 1 OFFSET 0)) AS ventas, " \
-          f" (SELECT precioUnitario FROM precio_unitario WHERE idproducto=p.id ORDER BY id DESC LIMIT 1 OFFSET 0) as precioUnitario, " \
-          f" (SELECT imagen FROM images WHERE idproducto=p.id ORDER BY id DESC LIMIT 1 OFFSET 0) as imagen " \
-          f"FROM productos AS p " \
-          f"INNER JOIN categorias as cats ON cats.id = p.idcategoria " \
-          f"WHERE p.estado=true and p.idcategoria={id_categoria} " \
-          f"ORDER BY precioUnitario DESC, ventas DESC;"
-    products = [dict(p) for p in db.select_multiple(sql)]
-    return products
+    products = get_all_products()
+    categorias = dict(map(lambda x: (x["id"], x), get_all_categories_for_catalog()))
+    products = [p for p in products if p['estado'] and p['idcategoria'] == categorias[id_categoria]["categoria"]]
+    # sql = f"SELECT p.id, p.descripcion, cats.categoria AS categoria, " \
+    #       f"(SELECT count(id) FROM ventas_productos WHERE idproducto=p.id " \
+    #       f"and idestado<>(SELECT id FROM estados where productos='1' ORDER BY id DESC LIMIT 1 OFFSET 0)) AS ventas, " \
+    #       f" (SELECT precioUnitario FROM precio_unitario WHERE idproducto=p.id ORDER BY id DESC LIMIT 1 OFFSET 0) as precioUnitario, " \
+    #       f" (SELECT imagen FROM images WHERE idproducto=p.id ORDER BY id DESC LIMIT 1 OFFSET 0) as imagen " \
+    #       f"FROM productos AS p " \
+    #       f"INNER JOIN categorias as cats ON cats.id = p.idcategoria " \
+    #       f"WHERE p.estado=true and p.idcategoria={id_categoria} " \
+    #       f"ORDER BY precioUnitario DESC, ventas DESC;"
+    # products = [dict(p) for p in db.select_multiple(sql)]
+    return products[:20]
 
 
 def get_featured_products(limit=20):
-    sql = f"SELECT p.id, p.descripcion, cats.categoria AS categoria, " \
-          f"(SELECT count(id) FROM ventas_productos WHERE idproducto=p.id " \
-          f"and idestado<>(SELECT id FROM estados where productos='1' ORDER BY id DESC LIMIT 1 OFFSET 0)) AS ventas, " \
-          f" (SELECT precioUnitario FROM precio_unitario WHERE idproducto=p.id ORDER BY id DESC LIMIT 1 OFFSET 0) as precioUnitario, " \
-          f" (SELECT imagen FROM images WHERE idproducto=p.id ORDER BY id DESC LIMIT 1 OFFSET 0) as imagen " \
-          f"FROM productos AS p " \
-          f"INNER JOIN categorias as cats ON cats.id = p.idcategoria and cats.catalogo=true " \
-          f"WHERE p.estado=true " \
-          f"ORDER BY ventas DESC, precioUnitario DESC LIMIT {limit} OFFSET 0;"
-    products = [dict(p) for p in db.select_multiple(sql)]  # if p["ventas"] > 0]
-    return products
+    products = get_all_products()
+    products = [p for p in products if p['estado'] and p['idcategoria']]
+    # sql = f"SELECT p.id, p.descripcion, cats.categoria AS categoria, " \
+    #       f"(SELECT count(id) FROM ventas_productos WHERE idproducto=p.id " \
+    #       f"and idestado<>(SELECT id FROM estados where productos='1' ORDER BY id DESC LIMIT 1 OFFSET 0)) AS ventas, " \
+    #       f" (SELECT precioUnitario FROM precio_unitario WHERE idproducto=p.id ORDER BY id DESC LIMIT 1 OFFSET 0) as precioUnitario, " \
+    #       f" (SELECT imagen FROM images WHERE idproducto=p.id ORDER BY id DESC LIMIT 1 OFFSET 0) as imagen " \
+    #       f"FROM productos AS p " \
+    #       f"INNER JOIN categorias as cats ON cats.id = p.idcategoria and cats.catalogo=true " \
+    #       f"WHERE p.estado=true " \
+    #       f"ORDER BY ventas DESC, precioUnitario DESC LIMIT {limit} OFFSET 0;"
+    # products = [dict(p) for p in db.select_multiple(sql)]  # if p["ventas"] > 0]
+    return products[:limit]
