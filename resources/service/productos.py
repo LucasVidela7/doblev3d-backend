2496b8ffefa7de7a8659904c801232a2f516ab2a
Lucas Videla <videlalucasandres@gmail.com>
2024-08-20 12:13:29 -0300

    Cantidad de productos

--- resources/service/productos.py
+++ resources/service/productos.py
@@ -77,8 +77,7 @@ def get_all_products():
     if products is None:
         sql = f"""
         SELECT p.*, cats.id AS idcategoria, cats.categoria AS categoria, 
-        (SELECT count(id) FROM ventas_productos WHERE idproducto=p.id and 
-        idestado<>(SELECT id FROM estados where productos='1' ORDER BY id DESC LIMIT 1 OFFSET 0)) AS ventas,  
+        (SELECT (SELECT COALESCE(SUM(cantidad),0)) FROM ventas_productos WHERE idproducto=p.id) AS ventas,  
         pu.precioUnitario as precioUnitario,
         CAST(CASE WHEN pu.fechaActualizacion + {int(prices_db()['diasVencimiento'])} < CURRENT_DATE THEN true ELSE false END AS boolean) AS precioUnitarioVencido,
         (SELECT imagen FROM images WHERE idproducto=p.id ORDER BY id DESC LIMIT 1 OFFSET 0) as imagen 
