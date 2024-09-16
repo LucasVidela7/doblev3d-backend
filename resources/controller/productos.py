756d5a5d76bfd457d26f7449f803be201a269e6c
Lucas Videla <videlalucasandres@gmail.com>
2024-09-16 11:04:53 -0300

    Fix precio pieza

--- resources/controller/productos.py
+++ resources/controller/productos.py
@@ -16,7 +16,7 @@ products_bp = Blueprint("routes-products", __name__)
 @products_bp.route('/productos/precioPieza', methods=['POST'])
 @token_required
 def agregar_producto():
-    pieza = calcular_precio_pieza(request.json)
+    pieza = products.calcular_precio_pieza(request.json)
     return jsonify({"status": bool(pieza), "pieza": pieza})
 
 
